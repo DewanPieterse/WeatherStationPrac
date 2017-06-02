1f53a0530cff9388517927157274a09eb2ed63d3
Dewan Pieterse <dewan.pieterse@gmail.com>
2017-06-02 10:44:00 +0200

    Completed
    Everything is done. And it works. Prac 3B done

--- main.c
+++ main.c
@@ -43,9 +43,10 @@
 // GLOBAL VARIABLES
 //====================================================================
 unsigned char isStarted = 0;
-unsigned char count = 0;				/*declare the count variable and
-										  initialising it to 0*/
-unsigned char *temp;
+unsigned char count = 0;			/*declare the count variable and
+									  initialising it to 0          */
+unsigned char bcd[16];					//Create an char array
+unsigned char *bcdptr;
 //====================================================================
 // FUNCTION DECLARATIONS
 //====================================================================
@@ -83,7 +84,6 @@ void initPorts(void)
 	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR2_0; 	//enable pull up for SW2
 	GPIOA->PUPDR |= GPIO_PUPDR_PUPDR3_0; 	//enable pull up for SW3
 }
-
 void Delay(void)
 {
 	volatile int i,j;
@@ -93,20 +93,20 @@ void Delay(void)
 
 unsigned char * ConverttoBCD(unsigned char countbcd)
 {
-	static unsigned char bcd[16];			//Create an char array
-	unsigned char *bcdptr;					//Create a char pointer
+
 	bcdptr = bcd;							//Point the pointer to the array address
+	unsigned int countbcdx = countbcd * 2;
 
-	unsigned char thousands, hundreds,tens,ones,decimals;
+	unsigned char thousands,hundreds,tens,ones,decimals;
 
-	thousands = ((countbcd / 1000) + 48);	//Get the thousands value
-	countbcd = countbcd % 1000;				//Modulus of countbcd to get rid of the thousands
-	hundreds = ((countbcd / 100) + 48);		//Get the hundreds value
-	countbcd = countbcd % 100;				//Modulus of countbcd to get rid of the hundreds
-	tens = ((countbcd / 10) + 48);			//Get the tens value
-	countbcd = countbcd % 10;				//Modulus of countbcd to get rid of the thousands
-	ones = ((countbcd / 1) + 48);			//Get the ones value
-	decimals = ((countbcd % 1) + 48);		//Get the decimal value
+	thousands = ((countbcdx / 10000) + 48);	//Get the thousands value
+	countbcdx = countbcdx % 10000;			//Modulus of countbcd to get rid of the thousands
+	hundreds = ((countbcdx / 1000) + 48);	//Get the hundreds value
+	countbcdx = countbcdx % 1000;			//Modulus of countbcd to get rid of the hundreds
+	tens = ((countbcdx / 100) + 48);		//Get the tens value
+	countbcdx = countbcdx % 100;			//Modulus of countbcd to get rid of the thousands
+	ones = ((countbcdx / 10) + 48);			//Get the ones value
+	decimals = ((countbcdx % 10) + 48);		//Get the decimal value
 
 	bcd[0] = thousands;						//Assign the values to the array
 	bcd[1] = hundreds;
@@ -114,6 +114,9 @@ unsigned char * ConverttoBCD(unsigned char countbcd)
 	bcd[3] = ones;
 	bcd[4] = 46;
 	bcd[5] = decimals;
+	bcd[6] = 32;							//Assign " mm" to array
+	bcd[7] = 109;
+	bcd[8] = 109;
 
 	return bcdptr;							//Return the array pointer
 
@@ -133,33 +136,31 @@ void main (void)
 		{
 			lcd_command(CLEAR);				// Clear lcd screen
 			lcd_putstring("Weather Station");// Write to lcd
-			lcd_command(LINE_TWO);		// Go to line 2 of the lcd
-			lcd_putstring("Press SW2");	// Write to lcd
-			isStarted = 1;				// Variable to keep track of whether button 0 is pressed
+			lcd_command(LINE_TWO);			// Go to line 2 of the lcd
+			lcd_putstring("Press SW2");		// Write to lcd
+			isStarted = 1;					// Variable to keep track of whether button 0 is pressed
 			Delay();
 		}
 
 		else if (isStarted && ((GPIOA->IDR & SW1) == 0))
 		{
-			count ++;					//Increments the count value by 1
-			GPIOB->ODR = count;			//Displaying count on the LEDs
-			lcd_command(CLEAR);			//Clears LCD before printing again
+			count ++;						//Increments the count value by 1
+			GPIOB->ODR = count;				//Displaying count on the LEDs
+			lcd_command(CLEAR);				//Clears LCD before printing again
 			lcd_putstring("Rain bucket tip");//Prints to LCD
-			Delay();					/*Delay to prevent multiple counts
-										  on one press of the button.		///////////*/
+			Delay();						/*Delay to prevent multiple counts
+										  	  on one press of the button.*/
 		}
 
 		else if (isStarted && ((GPIOA->IDR & SW2) == 0))
 		{
-			lcd_command(CLEAR);			//Clears LCD before printing again
-			lcd_putstring("Count:");	//Prints to LCD
-			lcd_command(LINE_TWO);		//go to line 2 of the lcd
-			//unsigned char rain = count * 0.2;//Multiply by 0.2 to represent rainfall for each button press.
-			temp = ConverttoBCD(count);	//sends the count to ConverttoBCD() function
-			lcd_putstring(temp);		//prints the returned ASCII string to lcd
+			lcd_command(CLEAR);				//Clears LCD before printing again
+			lcd_putstring("Rainfall:");		//Prints to LCD
+			lcd_command(LINE_TWO);			//go to line 2 of the lcd
+			lcd_putstring(ConverttoBCD(count));//prints the returned ASCII string to lcd
 			Delay();
 		}
 	}
-}										// End of main
+}											// End of main
 
 // END OF PROGRAM
